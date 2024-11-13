98ed2511d882215346bca920a139f9b42615a5f7
smrhp <smrhp@users.noreply.github.com>
2024-11-13 17:34:42 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -20,12 +20,12 @@ fi
 
 # installation fichiers privetcloud
 sudo mkdir /usr/local/share/privetcloud
-chown -R privetcloud: /usr/local/share/privetcloud
+sudo chown -R privetcloud: /usr/local/share/privetcloud
 cp -r src/usr/local/share/privetcloud /usr/local/share/privetcloud
 
 # installation anydesk
 wget -O /tmp/anydesk.deb https://download.anydesk.com/linux/anydesk_6.4.0-1_amd64.deb
-apt -f install /tmp/anydesk.deb
+apt -f -y install /tmp/anydesk.deb
 
 apt-get update && apt-get --force-yes dist-upgrade
 apt install -y vim remmina remmina-* conky-all openssh-server numlockx
