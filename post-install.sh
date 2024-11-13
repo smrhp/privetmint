1f591a9eac4fbff40d12c447b52f2382d461cd87
smrhp <smrhp@users.noreply.github.com>
2024-11-13 16:08:46 +0100

    Update post-install.sh
    ajout prompt username
    et copie de fichier conkyrc

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+read -p "Nom de l'utilisateur à créer :" username
+if [ -z "$username" ]; then
+  echo "Vous devez saisir un nom d'utilisateur"
+  return -1
+]
+sudo useradd -m $username
+# Vérification de la création de l'utilisateur
+if [ $? -eq 0 ]; then
+  # Utiliser le nom d'utilisateur comme mot de passe
+  echo "$username:$username" | sudo chpasswd
+  echo "L'utilisateur $username a été créé avec le mot de passe par défaut."
+  cp /src/templates/tpl_conky.conf /home/$username/.conkyrc
+else
+  echo "Erreur lors de la création de l'utilisateur."
+  return -1
+fi
+
 # installation fichiers privetcloud
-mkdir /usr/local/share/privetcloud
+sudo mkdir /usr/local/share/privetcloud
 cp -r src/usr/local/share/privetcloud /usr/local/share/privetcloud
 chown -R privetcloud: /usr/local/share/privetcloud
 
